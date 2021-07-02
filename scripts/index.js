5bac6dfc1ebe7cc2d2a97e2f5e6e1977853fd41a
Slyce233 <andrew.adamah@ontariotechu.net>
2021-07-02 18:27:35 -0400

    UI changes and added contact

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,29 @@
 window.onload = function(){
-    
+    $('#timeline').timelinev({
+
+        // container element
+        timelineDiv:   $(this),
+        
+        // selector of timeline content
+        containerDivs:   '.timelineCont',
+        
+        // selector of date
+        dateDiv:     '.date',
+        
+        // selector of event details
+        textDiv:     '.text',
+        
+        // HTML markup for dates
+        dateHtml:      'h2',
+        
+        // HTML markup for details
+        textHtml:      'p',
+        
+        // active class
+        dateActiveClass: '.active',
+        
+        // animation speed
+        datesSpeed:      '750'
+        
+        });
 }
